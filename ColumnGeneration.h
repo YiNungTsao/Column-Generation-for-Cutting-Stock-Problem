a3850cc8d2ae95998161d66a76e715a6aee029d7
Yi-Nung Tsao <39461286+YiNungTsao@users.noreply.github.com>
2023-07-19 18:10:53 +0800

    Update ColumnGeneration.h

--- ColumnGeneration.h
+++ ColumnGeneration.h
@@ -14,6 +14,8 @@ private:
 	
 	static IloNumArray dual_variables;
 	static double reduced_cost;
+	static const int NUM_ITEMS = 5;
+	static const int MAX_LENGTH = 110;
 
 	static void initial_column(std::vector<int> item_length);
 	static void MasterProblem(std::vector<int> item_length, std::vector<int> item_demand);
